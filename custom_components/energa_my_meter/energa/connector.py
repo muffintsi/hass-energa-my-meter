640a5c393bc9dc0896387e4f6d53ec5b7bc58726
muffintsi <74093450+muffintsi@users.noreply.github.com>
2025-12-04 16:12:08 +0100

    Update connector.py to accept all ssl certs

--- custom_components/energa_my_meter/energa/connector.py
+++ custom_components/energa_my_meter/energa/connector.py
@@ -8,19 +8,27 @@ import logging
 import urllib
 from datetime import timedelta, datetime
 from urllib.error import HTTPError
+import ssl
 
 import lxml.html
 import mechanize
 from homeassistant.util import dt as dt_util
 from mechanize import Browser
 
-from .const import ENERGA_MY_METER_DATA_URL, ENERGA_REQUESTS_TIMEOUT, \
-    ENERGA_HISTORICAL_DATA_URL, ENERGA_MY_METER_LOGIN_URL, ENERGA_ACCOUNT_DATA_URL
+from .const import (
+    ENERGA_MY_METER_DATA_URL,
+    ENERGA_REQUESTS_TIMEOUT,
+    ENERGA_HISTORICAL_DATA_URL,
+    ENERGA_MY_METER_LOGIN_URL,
+    ENERGA_ACCOUNT_DATA_URL,
+)
 from .data import EnergaStatisticsData
 from .errors import (
     EnergaWebsiteLoadingError,
     EnergaMyMeterAuthorizationError,
-    EnergaMyMeterCaptchaRequirementError, EnergaStatisticsCouldNotBeLoadedError, EnergaMyMeterWebsiteError
+    EnergaMyMeterCaptchaRequirementError,
+    EnergaStatisticsCouldNotBeLoadedError,
+    EnergaMyMeterWebsiteError,
 )
 from .scrapper import EnergaWebsiteScrapper
 from .stats_modes import EnergaStatsModes, EnergaStatsTypes
@@ -55,14 +63,22 @@ class EnergaWebsiteConnector:
         self._browser.close()
 
     def get_historical_consumption_for_day(
-            self, start_date: datetime, meter_id: int, mode: EnergaStatsModes,
-            tariff_name: str | None = None
+        self,
+        start_date: datetime,
+        meter_id: int,
+        mode: EnergaStatsModes,
+        tariff_name: str | None = None,
     ) -> EnergaStatisticsData:
         """Returns the historical consumption of the meter for the specified day"""
         return self._get_statistic_for_date(start_date, EnergaStatsTypes.DAY, meter_id, mode, tariff_name)
 
-    def get_first_historical_consumption_for_type(self, start_date: datetime, meter_id: int, mode: EnergaStatsModes,
-                                                  stat_type: EnergaStatsTypes) -> datetime | None:
+    def get_first_historical_consumption_for_type(
+        self,
+        start_date: datetime,
+        meter_id: int,
+        mode: EnergaStatsModes,
+        stat_type: EnergaStatsTypes,
+    ) -> datetime | None:
         """Returns the first historical consumption of the meter for the specified type"""
         current_period = start_date
         result: datetime | None = None
@@ -70,24 +86,35 @@ class EnergaWebsiteConnector:
         try:
             while retries < 10:
                 retries += 1
-                _LOGGER.debug('Searching for the first %s statistic in the period %s (%s)', stat_type.value,
-                              current_period.strftime('%Y/%m/%d'), int(current_period.timestamp()) * 1000)
+                _LOGGER.debug(
+                    "Searching for the first %s statistic in the period %s (%s)",
+                    stat_type.value,
+                    current_period.strftime("%Y/%m/%d"),
+                    int(current_period.timestamp()) * 1000,
+                )
                 response = self._get_statistic_for_date(
-                    current_period.replace(hour=0, minute=0, second=0, microsecond=0), stat_type, meter_id, mode)
+                    current_period.replace(hour=0, minute=0, second=0, microsecond=0),
+                    stat_type,
+                    meter_id,
+                    mode,
+                )
                 tz = dt_util.get_time_zone(response.timezone)
 
                 if len(response.historical_points) == 0:
                     _LOGGER.debug(
                         "No historical points found in this period. The previous one contained the result: %s",
-                        result.strftime('%Y/%m/%d')
+                        result.strftime("%Y/%m/%d") if result else "None",
                     )
                     break
 
                 non_empty = response.get_first_non_empty_stat()
                 if non_empty:
                     result = non_empty.get_date(tz=tz)
-                    _LOGGER.debug("A new first non-empty %s statistic found for %s.", stat_type.value,
-                                  result.strftime('%Y/%m/%d'))
+                    _LOGGER.debug(
+                        "A new first non-empty %s statistic found for %s.",
+                        stat_type.value,
+                        result.strftime("%Y/%m/%d"),
+                    )
 
                 # Check the previous results if possible
                 first_stat = response.historical_points[0]
@@ -105,39 +132,58 @@ class EnergaWebsiteConnector:
 
             if result:
                 _LOGGER.debug(
-                    'The first found %s statistic is %s',
-                    stat_type.value, result.strftime('%Y/%m/%d')
+                    "The first found %s statistic is %s",
+                    stat_type.value,
+                    result.strftime("%Y/%m/%d"),
                 )
                 return result
             return None
 
         except (HTTPError, urllib.error.URLError) as error:
-            _LOGGER.error('Got an error response from the energa website when getting historical stats %s (id: %s): %s',
-                          ENERGA_HISTORICAL_DATA_URL, meter_id, error)
+            _LOGGER.error(
+                "Got an error response from the energa website when getting historical stats %s (id: %s): %s",
+                ENERGA_HISTORICAL_DATA_URL,
+                meter_id,
+                error,
+            )
             raise EnergaWebsiteLoadingError from error
 
-    def _get_statistic_for_date(self, start_date: datetime, stat_type: EnergaStatsTypes, meter_id: int,
-                                mode: EnergaStatsModes, tariff_name: str | None = None) -> EnergaStatisticsData:
+    def _get_statistic_for_date(
+        self,
+        start_date: datetime,
+        stat_type: EnergaStatsTypes,
+        meter_id: int,
+        mode: EnergaStatsModes,
+        tariff_name: str | None = None,
+    ) -> EnergaStatisticsData:
         """Returns the data returned by Energa when asking for a specific statistic period"""
         try:
             request_data = {
-                'mainChartDate': int(start_date.timestamp() * 1000),
-                'type': stat_type.value,
-                'meterPoint': meter_id,
-                'mo': mode.value
+                "mainChartDate": int(start_date.timestamp() * 1000),
+                "type": stat_type.value,
+                "meterPoint": meter_id,
+                "mo": mode.value,
             }
             if tariff_name:
-                request_data['tariffName'] = tariff_name
-            request = mechanize.Request(url=ENERGA_HISTORICAL_DATA_URL, method='GET', data=request_data)
+                request_data["tariffName"] = tariff_name
+            request = mechanize.Request(
+                url=ENERGA_HISTORICAL_DATA_URL,
+                method="GET",
+                data=request_data,
+            )
             response = self._browser.open(request, timeout=ENERGA_REQUESTS_TIMEOUT)
             json_response = response.read()
             result = json.loads(json_response)
-            if result is None or not result.get('success'):
+            if result is None or not result.get("success"):
                 raise EnergaStatisticsCouldNotBeLoadedError
-            return EnergaStatisticsData(result.get('response'))
+            return EnergaStatisticsData(result.get("response"))
         except (HTTPError, urllib.error.URLError) as error:
-            _LOGGER.error('Got an error response from the energa website %s (id: %s): %s',
-                          ENERGA_HISTORICAL_DATA_URL, meter_id, error)
+            _LOGGER.error(
+                "Got an error response from the energa website %s (id: %s): %s",
+                ENERGA_HISTORICAL_DATA_URL,
+                meter_id,
+                error,
+            )
             raise EnergaWebsiteLoadingError from error
 
     def _authorize_user(self, username: str, password: str):
@@ -145,32 +191,40 @@ class EnergaWebsiteConnector:
         login_page = self._open_page(ENERGA_MY_METER_LOGIN_URL)
 
         token = EnergaWebsiteScrapper.get_xrf_token(login_page)
-        request = mechanize.Request(url=ENERGA_MY_METER_LOGIN_URL, method='POST', data={
-            'selectedForm': 1,
-            'save': 'save',
-            '_antixsrf': token,
-            'clientOS': 'web',
-            'j_username': username,
-            'j_password': password,
-            'rememberMe': 'on',
-            'loginNow': 'zaloguj się'
-        })
+        request = mechanize.Request(
+            url=ENERGA_MY_METER_LOGIN_URL,
+            method="POST",
+            data={
+                "selectedForm": 1,
+                "save": "save",
+                "_antixsrf": token,
+                "clientOS": "web",
+                "j_username": username,
+                "j_password": password,
+                "rememberMe": "on",
+                "loginNow": "zaloguj się",
+            },
+        )
         return self._open_page(request)
 
     def open_home_page(self, meter_id: int | None = None, ppe: int | None = None):
         """Opens the main view of Energa My Meter that contains most of the information"""
         request_data = {}
         if meter_id and ppe:
-            request_data['mpc'] = meter_id
-            request_data['ppe'] = ppe
-        request = mechanize.Request(url=ENERGA_MY_METER_DATA_URL, method='GET', data=request_data)
+            request_data["mpc"] = meter_id
+            request_data["ppe"] = ppe
+        request = mechanize.Request(
+            url=ENERGA_MY_METER_DATA_URL,
+            method="GET",
+            data=request_data,
+        )
         html_result = self._open_page(request)
         self._verify_logged_in(html_result)
         return html_result
 
     def open_account_page(self):
         """Opens the main view of Energa My Meter that contains the list of meters configured for the account"""
-        request = mechanize.Request(url=ENERGA_ACCOUNT_DATA_URL, method='GET')
+        request = mechanize.Request(url=ENERGA_ACCOUNT_DATA_URL, method="GET")
         html_result = self._open_page(request)
         self._verify_logged_in(html_result)
         return html_result
@@ -184,7 +238,7 @@ class EnergaWebsiteConnector:
             else:
                 raise EnergaWebsiteLoadingError
         except (HTTPError, urllib.error.URLError) as error:
-            _LOGGER.error('Got an error response from the energa website %s: %s', url, error)
+            _LOGGER.error("Got an error response from the energa website %s: %s", url, error)
             raise EnergaWebsiteLoadingError from error
         result = self._parse_response(html_response)
 
@@ -217,4 +271,9 @@ class EnergaWebsiteConnector:
         browser.set_handle_robots(False)
         browser.set_handle_equiv(False)
         browser.set_handle_refresh(False)
+
+        # 🔑 Wyłączamy weryfikację SSL tylko dla tego Browsera
+        ctx = ssl._create_unverified_context()
+        browser.set_ca_data(context=ctx)
+
         return browser
